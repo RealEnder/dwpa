45c50a3d19858d763cbaaf20c2c549ea42d523df
Alex Stanev <alex@stanev.org>
2011-08-23 06:40:26 +0000

    First static version

---
+++ web/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<head>
+
+<meta http-equiv="Content-type" content="text/html; charset=UTF-8" />
+<meta name="description" content="besside-ng wpa crack" />
+<meta name="keywords" content="besside-ng, wpa, crack, wordlist" />
+<meta name="author" content="Alex Stanev, http://sec.stanev.org" />
+
+<title>Free online WPA cracker with stats - besside-ng companion</title>
+
+<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+
+</head>
+<body>
+
+<a name="top"></a>
+
+<div id="header">Free online WPA cracker</div>
+
+<ul id="navtop">
+<li><a href="index.html">Home</a></li>
+<li><a href="#sitename">Submit</a></li>
+<li><a href="#sitename">Networks</a></li>
+<li><a href="#sitename">Dicts</a></li>
+<li><a href="#sitename">Stats</a></li>
+<li style="float:right;"><form action="" method="get">Search nets <input class="searchinput" type="text" id="search" value="" /></form></li>
+</ul>
+<div id="maincontainer">
+
+<div id="contentwrapper">
+<div id="contentcolumn">
+
+<p>
+<h1>Online WPA cracker with stats - besside-ng companion</h1>
+Contribute to WPA security research - the more handshakes you upload, the more
+stats, and the more we'll understand how feasible WPA cracking is in practice.
+</p>
+
+<p>
+<h4>Usage</h4>
+To obtain the WPA handshake, use besside-ng (from <a href="http://www.aircrack-ng.org/downloads.html">aircrack-ng</a>'s SVN), a tool that will automatically own all the WPA networks it finds. If you have Internet connectivity while running besside-ng, use the -s wpa.darkircop.org option to upload wpa.cap automatically.
+</p>
+
+<p>
+<h4>Distributed WPA cracking</h4>
+Contribute your CPU to the cracking process by running <a href="http://wpa.darkircop.org/help_crack.sh">help_crack.sh</a> (v8), a script that will automatically fetch uncracked networks, try to crack them, and upload the results to this site. If you want to use your own wordlist, pass it as an argument. 
+</p>
+
+</div>
+</div>
+
+<div id="footer">
+<div class="hr"><hr /></div>
+Contact: sorbo at darkircop dot org, alex at stanev dot org
+</div>
+
+</div>
+</body>
+</html>
