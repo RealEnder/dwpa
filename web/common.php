b333a984e6ca5a1b8b6b42dc4721ef1e528f7d36
Alex Stanev <alex@stanev.org>
2011-08-24 18:34:12 +0000

    Write key to db

--- web/common.php
+++ web/common.php
@@ -3,20 +3,21 @@ define('AIRCRACK', 'aircrack-ng');
 define('WPACLEAN', '/var/www/dev/wpa/wpaclean');
 define('WPA_CAP', '/var/www/dev/wpa/wpa.cap');
 
+//Execute aircrack-ng and check for solved net
 function check_pass($bssid, $pass) {
     $wl = '/tmp/wl';
     $kf = '/tmp/key';
 
-    if (strlen($pass) <= 8)
+    if (strlen($pass) < 8)
         return false;
 
+    @unlink($kf);
     file_put_contents($wl, $pass."\n");
 
-    unlink($kf);
     $x = AIRCRACK." -b $bssid -w $wl -l $kf ".WPA_CAP;
-    exec($x, $out);
+    exec($x);
 
-    $p = file_get_contents($kf);
+    $p = @file_get_contents($kf);
 
     return ($p == $pass);
 }
--- web/content/nets.php
+++ web/content/nets.php
@@ -6,34 +6,59 @@ if (empty($_POST)): ?>
 <table style="border: 1;">
 <tr><th>BSSID</th><th>SSID</th><th>WPA key</th><th>Timestamp</th></tr>
 <?
-$sql = 'SELECT * FROM nets ORDER BY ts DESC LIMIT 20';
-$stmt = mysqli_stmt_init($mysql);
-mysqli_stmt_prepare($stmt, $sql);
-mysqli_stmt_execute($stmt);
-$data = array();
-stmt_bind_assoc($stmt, $data);
-while ($stmt->fetch()) {
-    $bssid = str_pad(dechex($data['bssid']), 12, '0', STR_PAD_LEFT);
-    $bssid = "{$bssid[0]}{$bssid[1]}:{$bssid[2]}{$bssid[3]}:{$bssid[4]}{$bssid[5]}:{$bssid[6]}{$bssid[7]}:{$bssid[8]}{$bssid[9]}:{$bssid[10]}{$bssid[11]}";
-    $ssid = htmlspecialchars($data['ssid']);
-    if ($data['pass'] == '') {
-        $pass = '<input class="input" type="text" name="'.$bssid.'" size="20"/>';
-    } else
-        $pass = htmlspecialchars($data['pass']);
-    $ts = $data['ts'];
-    echo "<tr><td style=\"font:Courier;\">$bssid</td><td>$ssid</td><td>$pass</td><td>$ts</td></tr>\n";
-}
-$stmt->close();
-$mysql->close();
+    $sql = 'SELECT * FROM nets ORDER BY ts DESC LIMIT 20';
+    $stmt = mysqli_stmt_init($mysql);
+    mysqli_stmt_prepare($stmt, $sql);
+    mysqli_stmt_execute($stmt);
+    $data = array();
+    stmt_bind_assoc($stmt, $data);
+    while ($stmt->fetch()) {
+        $bssid = str_pad(dechex($data['bssid']), 12, '0', STR_PAD_LEFT);
+        $bssid = "{$bssid[0]}{$bssid[1]}:{$bssid[2]}{$bssid[3]}:{$bssid[4]}{$bssid[5]}:{$bssid[6]}{$bssid[7]}:{$bssid[8]}{$bssid[9]}:{$bssid[10]}{$bssid[11]}";
+        $ssid = htmlspecialchars($data['ssid']);
+        if ($data['pass'] == '') {
+            $pass = '<input class="input" type="text" name="'.$bssid.'" size="20"/>';
+        } else
+            $pass = htmlspecialchars($data['pass']);
+        $ts = $data['ts'];
+        echo "<tr><td style=\"font:Courier;\">$bssid</td><td>$ssid</td><td>$pass</td><td>$ts</td></tr>\n";
+    }
 ?>
 </table>
 <input class="submitbutton" type="submit" value="Send WPA keys" />
 </form>
 <? else:
-//var_dump($_POST);
-foreach ($_POST as $name => $value) {
-    if (preg_match('/([a-f0-9]{2}:?){6}/',$value))
-        echo "$value";
-}
+    require('common.php');
+
+    //Check stmt
+    $sql = 'SELECT * FROM nets WHERE bssid = ? AND n_state=0';
+    $stmt = $mysql->stmt_init();
+    $stmt->prepare($sql);
+    $data = array();
+    stmt_bind_assoc($stmt, $data);
+
+    //Update key stmt
+    $usql = 'UPDATE nets SET pass=?, sip=?, n_state=1, sts=NOW() WHERE bssid=?';
+    $ustmt = $mysql->stmt_init();
+    $ustmt->prepare($usql);
+
+    foreach ($_POST as $bssid => $key) {
+        if (preg_match('/([a-f0-9]{2}:?){6}/', $bssid) && strlen($key) >= 8) {
+            $ibssid = hexdec(str_replace(':', '', $bssid));
+            $stmt->bind_param('i', $ibssid);
+            $stmt->execute();
+
+            if ($stmt->fetch())
+                if (check_pass($bssid, $key)) {
+                    $stmt->free_result();
+                    $iip = ip2long($_SERVER['REMOTE_ADDR']);
+                    $ustmt->bind_param('sii', mysqli_real_escape_string($mysql, $key), $iip, $ibssid);
+                    $ustmt->execute();
+                }
+        }
+    }
+    $ustmt->close();
 endif;
+$stmt->close();
+$mysql->close();
 ?>
