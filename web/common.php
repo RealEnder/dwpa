75163cea92a240885df3f98e1f55269d47fa2547
Alex Stanev <alex@stanev.org>
2011-08-25 15:38:29 +0000

    commonize net table

--- web/common.php
+++ web/common.php
@@ -36,4 +36,28 @@ function long2mac($lmac) {
 function valid_mac($mac) {
     return preg_match('/([a-f0-9]{2}:?){6}/', strtolower($mac));
 }
+
+//Write nets table
+function write_nets($stmt, $data) {
+    $has_input = false;
+    echo '
+<form class="form" method="POST" action="?nets" enctype="multipart/form-data">
+<table style="border: 1;">
+<tr><th>BSSID</th><th>SSID</th><th>WPA key</th><th>Timestamp</th></tr>';
+    while ($stmt->fetch()) {
+        $bssid = long2mac($data['bssid']);
+        $ssid = htmlspecialchars($data['ssid']);
+        if ($data['pass'] == '') {
+            $pass = '<input class="input" type="text" name="'.$bssid.'" size="20"/>';
+            $has_input = true;
+        } else
+            $pass = htmlspecialchars($data['pass']);
+        $ts = $data['ts'];
+        echo "<tr><td style=\"font:Courier;\">$bssid</td><td>$ssid</td><td>$pass</td><td>$ts</td></tr>\n";
+    }
+    echo '</table>';
+    if ($has_input)
+        echo '<input class="submitbutton" type="submit" value="Send WPA keys" />';
+    echo '</form>';
+}
 ?>
