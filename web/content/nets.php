192f16e7cc050dfaae2776705cd8a9c06429d132
Alex Stanev <alex@stanev.org>
2011-08-25 14:03:31 +0000

    refactore

--- web/content/nets.php
+++ web/content/nets.php
@@ -1,33 +1,7 @@
 <?
 require('db.php');
 require('common.php');
-if (empty($_POST)): ?>
-<h1>Last 20 submitted networks</h1>
-<form class="form" method="POST" action="?nets" enctype="multipart/form-data">
-<table style="border: 1;">
-<tr><th>BSSID</th><th>SSID</th><th>WPA key</th><th>Timestamp</th></tr>
-<?
-    $sql = 'SELECT * FROM nets ORDER BY ts DESC LIMIT 20';
-    $stmt = $mysql->stmt_init();
-    $stmt->prepare($sql);
-    $stmt->execute();
-    $data = array();
-    stmt_bind_assoc($stmt, $data);
-    while ($stmt->fetch()) {
-        $bssid = long2mac($data['bssid']);
-        $ssid = htmlspecialchars($data['ssid']);
-        if ($data['pass'] == '') {
-            $pass = '<input class="input" type="text" name="'.$bssid.'" size="20"/>';
-        } else
-            $pass = htmlspecialchars($data['pass']);
-        $ts = $data['ts'];
-        echo "<tr><td style=\"font:Courier;\">$bssid</td><td>$ssid</td><td>$pass</td><td>$ts</td></tr>\n";
-    }
-?>
-</table>
-<input class="submitbutton" type="submit" value="Send WPA keys" />
-</form>
-<? else:
+if (!empty($_POST)) {
     //Check stmt
     $sql = 'SELECT * FROM nets WHERE bssid = ? AND n_state=0';
     $stmt = $mysql->stmt_init();
@@ -55,8 +29,19 @@ if (empty($_POST)): ?>
                 }
         }
     }
+    $stmt->close();
     $ustmt->close();
-endif;
+}
+
+echo '<h1>Last 20 submitted networks</h1>';
+$sql = 'SELECT * FROM nets ORDER BY ts DESC LIMIT 20';
+$stmt = $mysql->stmt_init();
+$stmt->prepare($sql);
+$stmt->execute();
+$data = array();
+stmt_bind_assoc($stmt, $data);
+write_nets($stmt, $data);
+
 $stmt->close();
 $mysql->close();
 ?>
