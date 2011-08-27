e6d128283f5af374410d0817c11f1843a6654816
Alex Stanev <alex@stanev.org>
2011-08-27 10:29:21 +0000

    return only bssid if cracking with local dict

--- web/content/get_work.php
+++ web/content/get_work.php
@@ -12,15 +12,26 @@ $stmt->execute();
 
 if ($stmt->fetch()) {
     $stmt->free_result();
-  
-    //Put nets
-    $usql = 'INSERT INTO n2d(bssid, d_id) VALUES(?, ?) ON DUPLICATE KEY UPDATE Hits=Hits+1';
-    $ustmt = $mysql->stmt_init();
-    $ustmt->prepare($usql);
-    $ustmt->bind_param('ii', $data['bssid'], $data['d_id']);
-    $ustmt->execute();
-    $ustmt->close();
-    echo long2mac($data['bssid'])."-{$data['dpath']}";
+
+    if ($_GET['get_work'] == 'no_dict') {
+        //Return net, to break with local dict
+        $usql = 'UPDATE nets SET hits=hist+1 WHERE bssid=?';
+        $ustmt = $mysql->stmt_init();
+        $ustmt->prepare($usql);
+        $ustmt->bind_param('i', $data['bssid']);
+        $ustmt->execute();
+        $ustmt->close();
+        echo long2mac($data['bssid']);
+    } else {
+        //Return net+dict
+        $usql = 'INSERT INTO n2d(bssid, d_id) VALUES(?, ?) ON DUPLICATE KEY UPDATE Hits=Hits+1';
+        $ustmt = $mysql->stmt_init();
+        $ustmt->prepare($usql);
+        $ustmt->bind_param('ii', $data['bssid'], $data['d_id']);
+        $ustmt->execute();
+        $ustmt->close();
+        echo long2mac($data['bssid'])."-{$data['dpath']}";
+    }
 } else {
     echo 'No nets';
 }
