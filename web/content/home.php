e8275d1573bf1bbb4e026175990c8f02fc7cdff5
Alex Stanev <alex@stanev.org>
2011-08-29 17:17:44 +0000

    write about cracking under windows

--- web/content/home.php
+++ web/content/home.php
@@ -9,5 +9,9 @@ To obtain the WPA handshake, use besside-ng (from <a href="http://www.aircrack-n
 
 <p>
 <h4>Distributed WPA cracking</h4>
-Contribute your CPU to the cracking process by running <a href="hc/help_crack.py">help_crack.py</a>, a script that will automatically fetch uncracked networks, try to crack them, and upload the results to this site. If you want to use your own wordlist, pass it as an argument. 
+Contribute your CPU to the cracking process by running <a href="hc/help_crack.py">help_crack.py</a>, a script that will automatically fetch uncracked networks, try to crack them, and upload the results to this site. If you want to use your own wordlist, pass it as an argument. Prerequisite: you must have python(tested with 2.7) and aircrack-ng installed.
+</p>
+<p>
+<h4>Cracking under Windows</h4>
+Install python 2.x from <a href="http://python.org/download/">here</a>. Then download precompiled binary of aircrack-ng - local version, with only commandline aircrack-ng inside <a href="hc/aircrack-ng-stripped-1.1-win.zip">here</a> or full distribution <a href="http://www.aircrack-ng.org/downloads.html">here</a>. Put aircrack-ng executables and <a href="hc/help_crack.py">help_crack.py</a> in same directory and run help_crack.py from cmd shell.
 </p>
