0e742c2d1ff0750044374c02aab6962730ab3974
Alex Stanev <alex@stanev.org>
2011-09-18 09:16:04 +0000

    add mail

--- web/content/get_key.php
+++ web/content/get_key.php
@@ -1,28 +1,8 @@
 <h1>Get key</h1>
 <?
-if ($recap_resp->is_valid) {
-    require('db.php');
-
-    $stats = array();
-    $sql = 'SELECT * FROM stats';
-    $stmt = $mysql->stmt_init();
-    $stmt->prepare($sql);
-    $data = array();
-    stmt_bind_assoc($stmt, $data);
-    $stmt->execute();
-
-    while ($stmt->fetch())
-        $stats[$data['pname']] = $data['pvalue'];
-    $stmt->close();
-    $mysql->close();
-
-    echo "Total nets: {$stats['nets']}<br/>\n";
-    echo "Cracked nets: {$stats['cracked']}<br/>\n";
-    if ((int) $stats['nets'] > 0) {
-        $srate = round((int) $stats['cracked'] / (int) $stats['nets'] * 100, 2);
-        echo "Success rate: $srate %<br/>\n";
-    }
-} else {
+if ($recap_resp->is_valid)
+    echo 'Key issued. Make sure you keep it to access the results.';
+else {
     require_once('recaptchalib.php');
     echo '
 Key is needed to see results for your uploaded captures. You may use one key with multiple uploads.<br/>
@@ -35,7 +15,8 @@ If you provide valid e-mail, results will be mailed when avaible (currently disa
     echo recaptcha_get_html($publickey, $recap_resp->error);
 
     echo'
-<br/>
+E-mail: <input class="searchinput" type="text" id="mail" name="mail" value="" />
+<br/><br/>
 <input class="submitbutton" type="submit" value="Get private key" />
 </form>';
 }
