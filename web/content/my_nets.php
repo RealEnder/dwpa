7e2274bccaaefc4168f880379ec86ad26bb7b342
Alex Stanev <alex@stanev.org>
2011-11-27 10:15:07 +0000

    use net_id for order

--- web/content/my_nets.php
+++ web/content/my_nets.php
@@ -14,7 +14,7 @@ if (isset($_COOKIE['key']))
 $sql = 'SELECT hex(nets.nhash) as nhash, nets.bssid AS bssid, nets.ssid AS ssid, nets.pass AS pass, nets.hits, nets.ts
 FROM nets, users
 WHERE nets.u_id=users.u_id AND users.ukey=?
-ORDER BY nets.ts DESC
+ORDER BY nets.net_id DESC
 LIMIT 20';
 
 $stmt = $mysql->stmt_init();
