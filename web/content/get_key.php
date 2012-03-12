4075ee608ce7aa29822ec49927dc6396380aca95
Alex Stanev <alex@stanev.org>
2012-03-12 06:59:25 +0000

    typos

--- web/content/get_key.php
+++ web/content/get_key.php
@@ -11,7 +11,7 @@ else {
         echo '
 Key is needed to see results for your uploaded handshakes. You may use one key with multiple uploads.<br/>
 If you provide valid e-mail, results will be mailed when avaible (currently disabled). This is not a mandatory field.<br/>
-When issued, the key will appear nest to search box and you can proceed with You can <a href="?submit">captures upload</a>.
+When issued, the key will appear next to search box and you can proceed with <a href="?submit">captures upload</a>.
 <script type="text/javascript">
     var RecaptchaOptions = {theme: "white"};
 </script>
