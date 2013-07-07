bc220076ff1f2ce06e0ce4b182ad2d70fd00d969
Alex Stanev <alex@stanev.org>
2013-07-07 14:05:14 +0000

    refactore logging

--- misc/check_db_vs_files.php
+++ misc/check_db_vs_files.php
@@ -6,6 +6,13 @@ require('common.php');
 $tmpcap = SHM.'check_db.cap';
 $log = 'cdb.log';
 
+function logit($mess) {
+    global $log;
+
+    echo "$mess\n";
+    file_put_contents($log, "$mess\n", FILE_APPEND);
+}
+
 $sql = 'SELECT net_id, bssid, HEX(nhash) AS nhash FROM nets';
 $stmt = $mysql->stmt_init();
 $stmt->prepare($sql);
@@ -21,8 +28,7 @@ while ($stmt->fetch())
 $stmt->close(); 
 $mysql->close();
 
-echo 'Got '.count($nets)." nets from db\n";
-file_put_contents($log, 'Got '.count($nets)." nets from db\n", FILE_APPEND);
+logit('Got '.count($nets).' nets from db');
 
 foreach ($nets as $net) {
     $net_id = $net[0]; 
@@ -32,33 +38,27 @@ foreach ($nets as $net) {
     //put decompressed cap in SHM
     file_put_contents($tmpcap, gzinflate(substr(file_get_contents(MD5CAPS.substr($nhash, 0, 3)."/$nhash.gz"), 10)));
     //check md5 hash vs nhash
-    if ($nhash != md5_file($tmpcap)) {
-        echo "Hash does not match! net_id: $net_id nhash:$nhash\n";
-        file_put_contents($log, "Hash does not match! net_id: $net_id nhash:$nhash\n", FILE_APPEND);
-    }
+    if ($nhash != md5_file($tmpcap))
+        logit("Hash does not match! net_id: $net_id nhash:$nhash");
+
     $pres = '';
     exec(PYRIT.' -r '.$tmpcap.' analyze', $pres, $rc);
     if ($rc == 0) {
         $spres = implode("\n", $pres);
         if (strpos($spres, 'got 1 AP(s)') === FALSE) {
-            echo "More nets found? net_id: $net_id nhash: $nhash\n";
-            echo $spres."\n";
-            file_put_contents($log, "More nets found? net_id: $net_id nhash: $nhash\n", FILE_APPEND);
-            file_put_contents($log, $spres."\n", FILE_APPEND);
+            logit("More nets found? net_id: $net_id nhash: $nhash");
+            logit("$spres\n");
         }
     } else {
-        echo "Pyrit failed: net_id:$net_id nhash:$nhash\n";
-        file_put_contents($log, "Pyrit failed: net_id:$net_id nhash:$nhash\n", FILE_APPEND);
-        file_put_contents($log, $spres."\n", FILE_APPEND);
+        logit("Pyrit failed: net_id:$net_id nhash:$nhash");
+        logit("$spres\n");
     }
-    if ($i % 1000 == 0) {
+    if ($i % 1000 == 0)
         echo "Net: $i\n";
-        file_put_contents($log, "Net: $i\n", FILE_APPEND);
-    }
+
     $i++;
     unlink($tmpcap);
 }
 
-echo 'Done';
-file_put_contents($log, 'Done', FILE_APPEND);
+logit('Done');
 ?>
