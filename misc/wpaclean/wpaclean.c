7133741bbe9789074355364495d5040e1803b9a1
Alex Stanev <alex@stanev.org>
2014-09-24 20:11:12 +0200

    remove commented code

--- misc/wpaclean/wpaclean.c
+++ misc/wpaclean/wpaclean.c
@@ -716,15 +716,9 @@ int main(int argc, char *argv[])
 
 	for (i = 2; i < argc; i++) {
 		char *in = argv[i];
-		//int prog = (int) (((double) (i - 1)) / ((double)(argc - 2)) 
-		//		   * 100.0);
-
-		//printf("Pwning %s (%d/%d %d%%)\n", in, i - 1, argc - 2, prog);
-		//fflush(stdout);
 
 		pwn(in);
 	}
 
-	//printf("Done\n");
 	exit(0);
 }
