0fa420f6158ad0446a11654404bfb5b71c9b44d4
leite <xxleite@gmail.com>
2015-08-16 05:58:25 -0300

    add paggination to 'my nets'

--- web/content/my_nets.php
+++ web/content/my_nets.php
@@ -6,25 +6,43 @@ put_work($mysql);
 
 echo '<h1>My networks</h1>';
 
-$k = '';
-if (isset($_COOKIE['key']))
-    if (valid_key($_COOKIE['key']))
-        $k = $_COOKIE['key'];
+$limit  = 20;
+$k      = (isset($_COOKIE['key']) && valid_key($_COOKIE['key'])) ? $_COOKIE['key'] : '';
+$offset = (isset($_GET['page']) && is_numeric($_GET['page'])) ? ((int)$_GET['page'] -1) * $limit : 0;
+$page   = ($offset / $limit) + 1;
 
-$sql = 'SELECT hex(nets.mic) as mic, nets.bssid AS bssid, nets.ssid AS ssid, nets.pass AS pass, nets.hits, n2u.ts
+$sql = 'SELECT SQL_CALC_FOUND_ROWS hex(nets.mic) as mic, nets.bssid AS bssid, nets.ssid AS ssid, nets.pass AS pass, nets.hits, n2u.ts
 FROM nets, n2u, users
 WHERE nets.net_id=n2u.net_id AND users.u_id=n2u.u_id AND users.userkey=UNHEX(?)
 ORDER BY nets.net_id DESC
-LIMIT 20';
+LIMIT ?,?';
+
+$total_sql = 'SELECT FOUND_ROWS()';
 
 $stmt = $mysql->stmt_init();
 $stmt->prepare($sql);
-$stmt->bind_param('s', $k);
-$stmt->execute();
+$stmt->bind_param('sii', $k, $offset, $limit);
+$ab = $stmt->execute();
 $data = array();
 stmt_bind_assoc($stmt, $data);
 write_nets($stmt, $data);
 
 $stmt->close();
+
+$stmt = $mysql->prepare($total_sql);
+$stmt->execute();
+$stmt->bind_result($total);
+$stmt->fetch();
+
+$stmt->close();
 $mysql->close();
+
+for ($i=1; $i<ceil($total / $limit) + 1; ++$i) {
+  if ($page == $i) {
+    echo "<span class='pagging active'>$i</span>";
+  } else {
+    echo "<a href='?my_nets&page=$i' class='pagging'>$i</a>";
+  }
+}
+
 ?>
