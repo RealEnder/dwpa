8d92f9ce01d9e4af31ef050453c9f4d3802ca802
Alex Stanev <alex@stanev.org>
2016-12-18 10:59:31 +0200

    Generate insert SQL for dicts

--- misc/create_gz.sh
+++ misc/create_gz.sh
@@ -1,9 +1,35 @@
 #!/bin/bash
-# Compress *.txt in current dir and create md5 checksum file
-gzip -9 *.txt
-for DICT in `ls *.gz`
+
+echo "Compress dictionaries(*.txt) in current dir and create inserts for dwpa"
+
+# get base URL
+URL=""
+REGEX="http://[-A-Za-z0-9+/%~_!.]*/"
+while [[ ! $URL =~ $REGEX ]]
 do
-    MD5=`md5sum $DICT`
+  read -p "Enter base URL for dict with trailing /: " URL
+done
+
+# check if we have cracked.txt and create it if missing
+if [ ! -f cracked.txt ]; then
+    echo "password" > cracked.txt
+fi
+
+# reset dict.sql
+echo -n "" > dict.sql
+
+# create inserts
+for DICT in `ls *.txt`
+do
+    # word count of dict
+    WC=$(wc -l < $DICT)
+    
+    # compress
+    gzip -9 < $DICT > $DICT.gz
+    
+    # md5 of gz dict
+    MD5=`md5sum < $DICT.gz`
     MD5="${MD5:0:32}"
-    echo -n "$MD5" > "$DICT.md5"
+    
+    echo "INSERT INTO dicts (dpath, dhash, dname, wcount, hits) VALUES ('$URL$DICT.gz', X'$MD5', '${DICT%.txt}', $WC, 0);" | tee -a dict.sql
 done
