ba89d0d3f98673c42a0702f0c631d5591604368b
Alex Stanev <alex@stanev.org>
2018-02-03 19:05:40 +0200

    Return only uncompressed hccapx, refactore

--- web/content/get_work.php
+++ web/content/get_work.php
@@ -1,53 +1,44 @@
 <?php
-if (version_compare($_GET['get_work'], MIN_HC_VER) < 0 )
+//check incoming version string
+if (version_compare($_GET['get_work'], MIN_HC_VER) < 0 ) {
     die('Version');
+}
 
 //check and validate options
-if (! array_key_exists('options', $_POST))
+if (! array_key_exists('options', $_POST)) {
     die('Options');
+}
 
 require_once('db.php');
 require_once('common.php');
-
-//avoid race condition
-$sem1 = sem_get(999);
-sem_acquire($sem1);
     
-//get work
-$sql = 'SELECT * FROM (SELECT * FROM onets LIMIT 1) a, (SELECT * FROM get_dict LIMIT 1) b';
-$stmt = $mysql->stmt_init();
-$stmt->prepare($sql);
-$data = array();
-stmt_bind_assoc($stmt, $data);
-$stmt->execute();
+//get next handshake/dict pair
+$result = $mysql->query('SELECT * FROM (SELECT * FROM onets LIMIT 1) a, (SELECT * FROM get_dict LIMIT 1) b');
+$data = $result->fetch_all(MYSQLI_ASSOC);
+$result->free();
 
-if ($stmt->fetch()) {
-    $stmt->free_result();
-    //return network data
-
-    $usql = 'INSERT INTO n2d(net_id, d_id) VALUES(?, ?) ON DUPLICATE KEY UPDATE Hits=Hits+1, ts=NOW()';
-    $ustmt = $mysql->stmt_init();
-    $ustmt->prepare($usql);
-    $ustmt->bind_param('ii', $data['net_id'], $data['d_id']);
-    $ustmt->execute();
-    $ustmt->close();
-
-    $json = array();
-    $json['mic']  = strtolower($data['mic']);
-    $json['bssid']  = long2mac($data['bssid']);
-    $json['dpath']  = $data['dpath'];
-    $json['dhash']  = strtolower($data['dhash']);
-    $json['cap']  = base64_encode($data['cap']);
-    $json['hccap']  = base64_encode($data['hccap']);
-
-    echo json_encode($json);
-} else {
-    echo 'No nets';
+if (count($data) != 1) {
+    $mysql->close();
+    die('No nets!?');
 }
+$data = $data[0];
 
-//release critical section
-sem_release($sem1);
-
-$stmt->close();
+//mark handshake/dict pair as returned for cracking
+$usql = 'INSERT INTO n2d(net_id, d_id) VALUES(?, ?) ON DUPLICATE KEY UPDATE Hits=Hits+1, ts=NOW()';
+$ustmt = $mysql->stmt_init();
+$ustmt->prepare($usql);
+$ustmt->bind_param('ii', $data['net_id'], $data['d_id']);
+$ustmt->execute();
+$ustmt->close();
 $mysql->close();
+
+//return to client
+$json = array();
+$json['hash']  = strtolower($data['hash']);
+$json['bssid']  = long2mac($data['bssid']);
+$json['dpath']  = $data['dpath'];
+$json['dhash']  = strtolower($data['dhash']);
+$json['hccapx']  = base64_encode($data['hccapx']);
+
+echo json_encode($json);
 ?>
