7140b285c744e678311a2a8a71195896332bd4ab
Alex Stanev <alex@stanev.org>
2018-02-03 21:02:40 +0200

    Move my_nets to new write_nets, refactore

--- web/content/my_nets.php
+++ web/content/my_nets.php
@@ -11,38 +11,35 @@ $k      = (isset($_COOKIE['key']) && valid_key($_COOKIE['key'])) ? $_COOKIE['key
 $offset = (isset($_GET['page']) && is_numeric($_GET['page'])) ? ((int)$_GET['page'] -1) * $limit : 0;
 $page   = ($offset / $limit) + 1;
 
-$sql = 'SELECT SQL_CALC_FOUND_ROWS hex(nets.hash) as hash, nets.bssid AS bssid, nets.ssid AS ssid, nets.pass AS pass, nets.hits, n2u.ts
+if ($k != '') {
+    $stmt = $mysql->stmt_init();
+    $stmt->prepare('SELECT SQL_CALC_FOUND_ROWS hex(nets.hash) as hash, nets.bssid AS bssid, nets.ssid AS ssid, nets.pass AS pass, nets.hits, n2u.ts
 FROM nets, n2u, users
 WHERE nets.net_id=n2u.net_id AND users.u_id=n2u.u_id AND users.userkey=UNHEX(?)
 ORDER BY nets.net_id DESC
-LIMIT ?,?';
+LIMIT ?,?');
+    $stmt->bind_param('sii', $k, $offset, $limit);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $datas = $result->fetch_all(MYSQLI_ASSOC);
+    $stmt->close();
 
-$total_sql = 'SELECT FOUND_ROWS()';
+    $result = $mysql->query('SELECT FOUND_ROWS()');
+    $total = $result->fetch_all(MYSQLI_NUM);
+    $result->free();
 
-$stmt = $mysql->stmt_init();
-$stmt->prepare($sql);
-$stmt->bind_param('sii', $k, $offset, $limit);
-$ab = $stmt->execute();
-$data = array();
-stmt_bind_assoc($stmt, $data);
-write_nets($stmt, $data);
+    $mysql->close();
 
-$stmt->close();
+    write_nets($datas);
 
-$stmt = $mysql->prepare($total_sql);
-$stmt->execute();
-$stmt->bind_result($total);
-$stmt->fetch();
-
-$stmt->close();
-$mysql->close();
-
-for ($i=1; $i<ceil($total / $limit) + 1; ++$i) {
-  if ($page == $i) {
-    echo "<span class='pagging active'>$i</span>";
-  } else {
-    echo "<a href='?my_nets&page=$i' class='pagging'>$i</a>";
-  }
+    //paging
+    $total = $total[0][0];
+    for ($i=1; $i<ceil($total / $limit) + 1; ++$i) {
+        if ($page == $i) {
+            echo "<span class='pagging active'>$i</span>";
+        } else {
+            echo "<a href='?my_nets&page=$i' class='pagging'>$i</a>";
+        }
+    }
 }
-
 ?>
