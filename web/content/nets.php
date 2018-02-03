faa4fca19e8c65be47eb0c34d1c24ff2ded1c136
Alex Stanev <alex@stanev.org>
2018-02-03 19:19:10 +0200

    Change mic to hash

--- web/content/nets.php
+++ web/content/nets.php
@@ -12,12 +12,12 @@ if (isset($_COOKIE['key']))
         $k = $_COOKIE['key'];
 
 if ($k == $bosskey)
-    $sql = 'SELECT hex(nets.mic) as mic, nets.bssid AS bssid, nets.ssid AS ssid, nets.pass AS pass, nets.hits, nets.ts
+    $sql = 'SELECT hex(nets.hash) AS hash, nets.bssid AS bssid, nets.ssid AS ssid, nets.pass AS pass, nets.hits, nets.ts
 FROM nets
 ORDER BY net_id DESC
 LIMIT 20';
 else
-    $sql = 'SELECT hex(nets.mic) as mic, nets.bssid AS bssid, nets.ssid AS ssid, IF(n.u_id IS NULL, IF(nets.pass IS NULL,NULL, \'Found\'), nets.pass) AS pass, nets.hits, nets.ts
+    $sql = 'SELECT hex(nets.hash) AS hash, nets.bssid AS bssid, nets.ssid AS ssid, IF(n.u_id IS NULL, IF(nets.pass IS NULL,NULL, \'Found\'), nets.pass) AS pass, nets.hits, nets.ts
 FROM (SELECT * FROM nets ORDER BY nets.net_id DESC LIMIT 20) AS nets
 LEFT JOIN (SELECT n2u.net_id AS net_id, users.u_id AS u_id FROM n2u, users WHERE n2u.u_id=users.u_id AND users.userkey=UNHEX(?) LIMIT 20) AS n ON n.net_id=nets.net_id';
 
