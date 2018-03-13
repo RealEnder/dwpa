c897a109d5fc65ee946fb98fd3556d85a5c5d6c1
Alex Stanev <alex@stanev.org>
2018-03-13 14:42:59 +0200

    NULL hkey on work package finish

--- web/content/put_work.php
+++ web/content/put_work.php
@@ -9,5 +9,14 @@ if (put_work($mysql, $_POST)) {
     echo 'Nope';
 }
 
+// mark by hkey in n2d
+if (isset($_POST['hkey']) && valid_key($_POST['hkey'])) {
+    $stmt = $mysql->stmt_init();
+    $stmt->prepare('UPDATE n2d SET hkey=NULL WHERE hkey=UNHEX(?)');
+    $stmt->bind_param('s', $_POST['hkey']);
+    $stmt->execute();
+    $stmt->close();
+}
+
 $mysql->close();
 ?>
