6aee883759f916ecd6ccfdb16889b99d7cd7082b
Alex Stanev <alex@stanev.org>
2018-08-27 16:13:17 +0300

    Releng help_crack 1.1.0

--- help_crack/help_crack.py
+++ help_crack/help_crack.py
@@ -56,7 +56,7 @@ conf = {
     'coptions': '',
     'dictcount': 1,
     'autodictcount': True,
-    'hc_ver': '1.0.2'
+    'hc_ver': '1.1.0'
 }
 conf['help_crack'] = conf['base_url'] + 'hc/help_crack.py'
 conf['help_crack_cl'] = conf['base_url'] + 'hc/CHANGELOG'
